43dc0597c635b9904bbdaa6f1320b8fcc607f4e8
Kinam Park <pkn@sk.com>
2025-08-01 09:51:17 +0900

    Task 2 헤더 컴포넌트 구현

--- main/src/components/Header/index.ts
+++ main/src/components/Header/index.ts
@@ -1,2 +1,5 @@
 // Header 컴포넌트를 export하는 인덱스 파일
-// export { default as Header } from './Header';
+export { default as Header } from './Header';
+export { default as Logo } from './Logo';
+export { default as DownloadButton } from './DownloadButton';
+export { default } from './Header';
